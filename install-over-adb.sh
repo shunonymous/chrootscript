f4320394fd8cca321d93270570fbe23bccffecdb
Shun Terabayashi <shunonymous@gmail.com>
2015-09-22 16:44:15 +0900

    fix mistake word

--- install-over-adb.sh
+++ install-over-adb.sh
@@ -10,7 +10,7 @@ adb push ./system /system
 adb shell chmod 755 /system/xbin/chrootinit
 adb shell chmod 755 /system/etc/init.d/75chrootset
 
-echo "Android system will reboot 10 seconds ago."
+echo "Android system will reboot 10 seconds later."
 echo "Cancel:Ctrl+c"
 
 sleep 10
